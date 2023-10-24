efa1c1c93a3846a152aa71a3a24490b28bc9d102
HideTake761 <55819922+HideTake761@users.noreply.github.com>
2023-10-25 08:52:51 +0900

    Add files via upload

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(to='/account/user/')
+            return redirect(to='/timeline/')
 
     else:
         form = SignupForm()
