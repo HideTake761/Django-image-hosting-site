03e1d137dd339853c0b78c1d9128c47b2d911fdc
HideTake761 <takenakahideto@hotmail.com>
2023-11-01 20:32:48 +0900

    20231101-5

--- timeline/urls.py
+++ timeline/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list, name='list'),
+    path('', views.list, name='list'), # urlとして'accounts:'は必要だが'timeline:'を書かなくていい理由。
     path('delete/<int:data_id>/', views.preview, name='delete'),
     path('create', views.create, name='create'),
     path('<int:num>', views.list, name='list'),
